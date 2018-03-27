4ddb1378e55da9a930f890fd015dd22af1d3ef66
kidziak <kidziak1996@gmail.com>
2018-03-27 15:56:55 +0200

    V2.8

--- CG_02_Dithering/CG_02_Dithering/Form1.cs
+++ CG_02_Dithering/CG_02_Dithering/Form1.cs
@@ -27,14 +27,14 @@ namespace CG_02_Dithering
         {
             int[] tresholds = new int[K];
             int ratio = greyvalueSum / (imageHeight * imageWidth);
-            for (int i = 0; i < tresholds.Length/2; i++)
+            for (int i = 0; i < tresholds.Length / 2; i++)
             {
                 tresholds[i] = 2 * i * ratio / K;
             }
 
             for (int i = 0; i < tresholds.Length / 2; i++)
             {
-                tresholds[i + tresholds.Length / 2] = ratio + 2* i * (255-ratio)/ K;
+                tresholds[i + tresholds.Length / 2] = ratio + 2 * i * (255 - ratio) / K;
             }
 
 
@@ -68,9 +68,9 @@ namespace CG_02_Dithering
 
         private int[,,] MakeColorCube(Bitmap outputImage)
         {
-            int [,,] cube = new int [64,64,64];
+            int[,,] cube = new int[64, 64, 64];
 
-            for(int i = 0; i<64; i++)
+            for (int i = 0; i < 64; i++)
             {
                 for (int j = 0; j < 64; j++)
                 {
@@ -86,7 +86,7 @@ namespace CG_02_Dithering
                 for (int y = 0; y < imageHeight; y++)
                 {
                     Color pixel = outputImage.GetPixel(x, y);
-                    cube[pixel.R/4, pixel.G/4, pixel.B/4]++;
+                    cube[pixel.R / 4, pixel.G / 4, pixel.B / 4]++;
                 }
             }
             return cube;
@@ -107,15 +107,15 @@ namespace CG_02_Dithering
                 Z = z;
                 Value = value;
             }
-         
+
         }
 
-        public class RGBA
+        public class RGB
         {
             public int R;
             public int G;
             public int B;
-            public RGBA(int r = 0, int g = 0, int b = 0)
+            public RGB(int r = 0, int g = 0, int b = 0)
             {
                 R = r;
                 G = g;
@@ -127,7 +127,7 @@ namespace CG_02_Dithering
         private List<Vector> FindMaximaOfCube(int[,,] cube)
         {
             List<Vector> maxima = new List<Vector>();
-            
+
 
             for (int i = 0; i < 64; i++)
             {
@@ -135,23 +135,23 @@ namespace CG_02_Dithering
                 {
                     for (int k = 0; k < 64; k++)
                     {
-                        if(maxima.Count != K)
+                        if (maxima.Count != K)
                         {
                             maxima.Add(new Vector(i, j, k, cube[i, j, k]));
                         }
 
                         else
                         {
-                            foreach(Vector vector in maxima)
+                            foreach (Vector vector in maxima)
                             {
-                                if(cube[i, j, k] > vector.Value)
+                                if (cube[i, j, k] > vector.Value)
                                 {
                                     maxima.Remove(vector);
                                     maxima.Add(new Vector(i, j, k, cube[i, j, k]));
                                     break;
                                 }
                             }
-                            
+
                         }
                     }
                 }
@@ -159,14 +159,14 @@ namespace CG_02_Dithering
 
             return maxima;
         }
- 
-        private List<RGBA> MakePalette(List<Vector> maxima)
-        {
-            List<RGBA> palette = new List<RGBA>();
 
-            foreach(Vector vector in maxima)
+        private List<RGB> MakePalette(List<Vector> maxima)
+        {
+            List<RGB> palette = new List<RGB>();
+
+            foreach (Vector vector in maxima)
             {
-                palette.Add(new RGBA(vector.X * 4, vector.Y * 4, vector.Z * 4));
+                palette.Add(new RGB(vector.X * 4, vector.Y * 4, vector.Z * 4));
             }
 
             return palette;
@@ -174,23 +174,27 @@ namespace CG_02_Dithering
 
         private double DistanceBetweenColors(int x1, int x2, int x3, int y1, int y2, int y3)
         {
-            return ((x1 - y1)*(x1 - y1) + (x2 - y2)*(x2 - y2) + (x3 - y3)*(x3 - y3));
+            int a = (x1 - y1);
+            int b = (x2 - y2);
+            int c = (x3 - y3);
+
+            return (a * a + b * b + c * c);
         }
 
-        private RGBA FindMinimalDistance(List<RGBA> palette, Color color)
+        private RGB FindMinimalDistance(List<RGB> palette, Color color)
         {
-            double max = 0;
-            RGBA maximal = new RGBA();
-            foreach(RGBA paletteColor in palette)
+            double min = 3 * 256 * 256;
+            RGB minimal = new RGB();
+            foreach (RGB paletteColor in palette)
             {
                 double distance = DistanceBetweenColors(paletteColor.R, paletteColor.G, paletteColor.B, color.R, color.G, color.B);
-                if (max < distance)
+                if (min > distance)
                 {
-                    max = distance;
-                    maximal = new RGBA(paletteColor.R, paletteColor.G, paletteColor.B);
+                    min = distance;
+                    minimal = new RGB(paletteColor.R, paletteColor.G, paletteColor.B);
                 }
             }
-            return maximal;
+            return minimal;
         }
 
 
@@ -198,14 +202,14 @@ namespace CG_02_Dithering
         {
             int[,,] cube = MakeColorCube(outputImage);
             List<Vector> maxima = FindMaximaOfCube(cube);
-            List<RGBA> palette = MakePalette(maxima);
+            List<RGB> palette = MakePalette(maxima);
 
             for (int x = 0; x < imageWidth; x++)
             {
                 for (int y = 0; y < imageHeight; y++)
                 {
                     Color pixel = outputImage.GetPixel(x, y);
-                    RGBA newPixel = FindMinimalDistance(palette, pixel);
+                    RGB newPixel = FindMinimalDistance(palette, pixel);
 
                     outputImage.SetPixel(x, y, Color.FromArgb(newPixel.R, newPixel.G, newPixel.B));
                 }
@@ -215,8 +219,96 @@ namespace CG_02_Dithering
             return outputImage;
         }
 
+        public class Centroid : RGB
+        {
+            public List<RGB> pixels;
+
+            public Centroid(int R = 0, int G = 0, int B = 0, List<RGB> p = null) : base(R, G, B)
+            {
+
+                pixels = new List<RGB>(p);
+            }
+        }
+
+        public List<Centroid> FindCentroids()
+        {
+            return new List<Centroid>();
+        }
+
+        public List<Centroid> RandomizeCentroids()
+        {
+            List<Centroid> centroids = new List<Centroid>();
+
+            for (int i = 0; i<K; i++)
+            {
+                Random random = new Random();
+                int R = random.Next(0, 256);
+                int G = random.Next(0, 256);
+                int B = random.Next(0, 256);
+
+
+                centroids.Add(new Centroid(R, G, B, null));
+            }
+
+            return centroids;
+        }
+
+        public RGB[,] ConvertBitmapToRGB(Bitmap image)
+        {
+            int width = image.Width;
+            int height = image.Height;
+            RGB[,] imageInRGB = new RGB[imageWidth, imageHeight];
+
+            for (int x = 0; x < imageWidth; x++)
+            {
+                for (int y = 0; y < imageHeight; y++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    imageInRGB[x, y] = new RGB(pixel.R, pixel.G, pixel.B);
+                }
+            }
+
+            return imageInRGB;
+        }
+
+        private Centroid FindMinimalDistance(List<Centroid> centroids, RGB color)
+        {
+            double min = 3 * 256 * 256;
+            Centroid minimal = new Centroid();
+            foreach (Centroid centroid in centroids)
+            {
+                double distance = DistanceBetweenColors(centroid.R, centroid.G, centroid.B, color.R, color.G, color.B);
+                if (min > distance)
+                {
+                    min = distance;
+                    //minimal = new RGB(paletteColor.R, paletteColor.G, paletteColor.B);
+                }
+            }
+            return minimal;
+        }
+
+        public void AssignPixlesToCentroid(List<Centroid> centroids, RGB[,] image)
+        {
+            for (int x = 0; x < imageWidth; x++)
+            {
+                for (int y = 0; y < imageHeight; y++)
+                {
+                    
+                }
+            }
+            
+        }
+
         private Bitmap kMeansColorQuantization(Bitmap outputImage)
         {
+            RGB[,] imageInPixles = ConvertBitmapToRGB(outputImage);
+            List<Centroid> centroids = RandomizeCentroids();
+
+
+
+
+
+
             return outputImage;
         }
 
